0bd0c317aa65822b42234998e445d8d1020b2d7e
Abhay Nigam <36666689+mashuk999@users.noreply.github.com>
2023-09-25 20:28:59 +0530

    Update tts_bark.py

--- tts_bark.py
+++ tts_bark.py
@@ -22,33 +22,8 @@ print('Started Generating Audio')
 # generate audio from text
 text_prompt = """[intro music]
 
-[clears throat] Good day, everyone. We've got some breaking news from Manipur that's sure to bring a sigh of relief to its residents. Chief Minister N Biren Singh has announced the restoration of mobile internet services in the state after nearly five long months of blackout.
-
-[laughs] Yes, you heard it right! The internet services, both mobile and broadband, were cut off in Manipur on May 3 due to the outbreak of violence. Initially, it was meant to be a short-lived measure, lasting just five days. But with the ongoing law-and-order concerns, the ban kept getting extended in five-day increments.
-
-Now, a member of CM Singh's team has informed The Indian Express that the decision to restore mobile internet services has been communicated to the relevant teams, and it's expected to be up and running within a few hours.
-
-[sighs]
-
-I can only imagine how the people of Manipur must have felt during these months of disconnectedness.
-
-[clears throat]
-
-The situation got so dire that even broadband services, which had been conditionally restored back in July, came with certain restrictions. The government was concerned about the spread of disinformation and false rumors through social media platforms. Bulk SMS was also a concern, as it could potentially mobilize agitators and demonstrators, posing risks to life and property.
-
-[gasps]
-
-But the winds of change seem to be blowing in Manipur. Just last month, the Manipur High Court directed the state government to find a way to provide internet services through mobile phones, albeit through a case-to-case whitelisting process and in phases.
-
-[music]
-
-[laughs] It appears the people's voices were heard, and connectivity is on its way back to the state. This is certainly a significant development, and we'll be keeping a close eye on the situation as it unfolds.
-
-[laughter]
-
-That's it for now, folks. Stay tuned for more updates, and remember, a connected world is a powerful world.
-
-[outro music]"""
+We've got some breaking news from Manipur that's sure to bring a sigh of relief to its residents.
+"""
 
 script = text_prompt.replace("\n", " ").strip()
 
